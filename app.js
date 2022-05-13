ca35e00ac158042d716889d4550955305146d58e
Sebastin J. Acosta S <boxofcodes.git@gmail.com>
2022-05-13 02:09:38 -0400

    Primer Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res)=>{
+	console.log('request start')
+	res.send('App Get , ok \n')
+})
+
+app.listen(port,()=>{
+	console.log(`Server Listen on Port: ${port}`)
+})
